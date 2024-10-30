5c32d594c0755a0063a6065dba5fbdd931aab68f
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-10-30 07:54:42 +0100

    Change string format

--- Mobilna2024/Mobilna2024/MainPage.xaml.cs
+++ Mobilna2024/Mobilna2024/MainPage.xaml.cs
@@ -10,9 +10,23 @@ namespace Mobilna2024
 {
     public partial class MainPage : ContentPage
     {
+        string[] napisy = { "Dzień dobry", "Good morning", "Buenos dias" };
+        int quoteIndex = 0;
         public MainPage()
         {
             InitializeComponent();
+
+            
+
+            cytat.Text = napisy[0];
+        }
+       
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+          quoteIndex++;
+            if (quoteIndex > napisy.Length)
+                quoteIndex = 0;
+            cytat.Text = napisy[quoteIndex];
         }
     }
 }
